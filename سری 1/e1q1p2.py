66716fb680a10e882f98fee13e2952df18fa72d3
Ali Bagheri Nejad <abneng1397@gmail.com>
2024-03-22 12:35:19 +0330

    1st attempt to add Kalhor to github

---
+++ سری 1/e1q1p2.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# The  W matrix
+W = np.array([[1, -1, 1, 1, 1, 1, 1],
+              [1, 1, 1, -1, -1, -1, -1],
+              [1, 1, 1, 1, 1, 1, 1],
+              [1, 1, -1, 1, 1, 1, 1]])/7
+             
+theta = np.array([6/7,3/7,1,6/7])
+
+def w_num(n):
+    
+    y = np.dot(W,n.T)
+    ans = theta-y
+    ans = np.abs(ans)<0.0001
+    return ans
